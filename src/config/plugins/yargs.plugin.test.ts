6f267e01b172f8e8dffce1ecee0ba468dd1d7507
Alejoboga20 <alejoboga19@gmail.com>
2024-06-06 17:38:55 -0300

    test custom args

--- src/config/plugins/yargs.plugin.test.ts
+++ src/config/plugins/yargs.plugin.test.ts
@@ -5,7 +5,20 @@ const runCommand = async (args: string[]) => {
 	return yarg;
 };
 
+const originalArgv = process.argv;
+
+const CUSTOM_BASE = 5;
+const CUSTOM_LIMIT = 20;
+const CUSTOM_SHOW_TABLE = true;
+const CUSTOM_NAME = 'custom-name';
+const CUSTOM_DIRECTORY = 'custom-directory';
+
 describe('Yargs Tests', () => {
+	beforeEach(() => {
+		process.argv = originalArgv;
+		jest.resetModules();
+	});
+
 	test('should return default values', async () => {
 		const yarg = await runCommand(['-b', '5']);
 
@@ -14,5 +27,25 @@ describe('Yargs Tests', () => {
 		expect(yarg.s).toBe(false);
 		expect(yarg.n).toBe('multiplication-table');
 		expect(yarg.d).toBe('outputs');
+	});
+
+	test('should return custom values', async () => {
+		const yarg = await runCommand([
+			'-b',
+			`${CUSTOM_BASE}`,
+			'-l',
+			`${CUSTOM_LIMIT}`,
+			'-s',
+			'-n',
+			`${CUSTOM_NAME}`,
+			'-d',
+			`${CUSTOM_DIRECTORY}`,
+		]);
+
+		expect(yarg.b).toBe(CUSTOM_BASE);
+		expect(yarg.l).toBe(CUSTOM_LIMIT);
+		expect(yarg.s).toBe(CUSTOM_SHOW_TABLE);
+		expect(yarg.n).toBe(CUSTOM_NAME);
+		expect(yarg.d).toBe(CUSTOM_DIRECTORY);
 	});
 });
