c9dbf4c4207ba2f2fff39a2154c6aa7c854b72f0
Alejoboga20 <alejoboga19@gmail.com>
2024-06-06 11:33:23 -0300

    test file save with custom values

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -5,6 +5,9 @@ const DEFAULT_FILE_NAME = 'table';
 const DEFAULT_FILE_DESTINATION = 'outputs';
 const FILE_CONTENT = 'test-content';
 
+const CUSTOM_FILE_NAME = 'custom-name';
+const CUSTOM_FILE_DESTINATION = 'custom-destination';
+
 const cleanUp = () => {
 	fs.rmSync(DEFAULT_FILE_DESTINATION, { recursive: true, force: true });
 };
@@ -26,6 +29,26 @@ describe('Save File Use Case', () => {
 		});
 
 		expect(saveFile).toBeInstanceOf(SaveFile);
+		expect(result).toBeTruthy();
+		expect(checkFile).toBeTruthy();
+		expect(fileContent).toEqual(options.fileContent);
+	});
+
+	test('should save a file with custom values', () => {
+		const options = {
+			fileContent: FILE_CONTENT,
+			fileName: CUSTOM_FILE_NAME,
+			fileDestination: CUSTOM_FILE_DESTINATION,
+		};
+
+		const saveFile = new SaveFile();
+		const result = saveFile.execute(options);
+
+		const checkFile = fs.existsSync(`${CUSTOM_FILE_DESTINATION}/${CUSTOM_FILE_NAME}.txt`);
+		const fileContent = fs.readFileSync(`${CUSTOM_FILE_DESTINATION}/${CUSTOM_FILE_NAME}.txt`, {
+			encoding: 'utf-8',
+		});
+
 		expect(result).toBeTruthy();
 		expect(checkFile).toBeTruthy();
 		expect(fileContent).toEqual(options.fileContent);
